eee3cd2fa1eca79d007a46cefe80151ccc992508
Luke Towers <luke@luketowers.ca>
2018-02-24 14:33:30 -0600

    Initial example of how to do validation

---
+++ validation.js
@@ -0,0 +1,16 @@
+function validate_email(email) {
+    var email_regex = /\S+@\S+\.\S+/;
+    return email_regex.test(email);
+}
+
+function validate_password(password) {
+
+}
+
+function validate_dateofbirth(dateofbirth) {
+
+}
+
+function validate_username(username) {
+
+}

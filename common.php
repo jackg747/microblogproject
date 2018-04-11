6b470b8c75c27c0b2758ce7f7334d5a0fb773b29
Luke Towers <luke@luketowers.ca>
2018-04-11 12:25:37 -0600

    Added support for testing user logins

--- common.php
+++ common.php
@@ -1,5 +1,12 @@
-<?php session_start();
-
+<?php
 require_once("vendor/password.php");
 require_once("validation.php");
 require_once("functions.php");
+
+$var = session_start();
+if (!$var) { throw new Exception("Sessions could not be started"); }
+
+$config = parse_ini_file('env.ini');
+if (!empty($config['FORCE_LOGIN_AS'])) {
+    $_SESSION['user_email'] = $config['FORCE_LOGIN_AS'];
+}
